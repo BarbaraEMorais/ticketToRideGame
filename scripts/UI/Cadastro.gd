924b00311fa900e83440bca4f8daeafa46930c0d
Bárbara Morais <barbara_morais@id.uff.br>
2025-05-25 14:56:56 -0300

    feat: improve bot names and set buttons functions - sign-up

--- scripts/UI/Cadastro.gd
+++ scripts/UI/Cadastro.gd
@@ -3,13 +3,19 @@ extends Control
 @onready var nomeJogador = $NomeJogador
 @onready var buttonCadastroJogador = $ButtonCadastroJogador
 @onready var feedbackCadastro = $FeedbackCadastro
+@onready var qtdJogadores = $QuantidadeJogadores.value
 
 var gerenciadorJogadores
 
 func _ready() -> void:
+	$QuantidadeJogadores.min_value = 2
+	$QuantidadeJogadores.max_value = 5
 	gerenciadorJogadores = load("res://scripts/jogadores/DadosJogadores.gd").new()
 	buttonCadastroJogador.pressed.connect(_on_cadastrar_pressed)
 	print("oi")
+
+func on_qtd_jogadores_changed(value):
+	print("num jogadores: ", value)
 
 func _on_cadastrar_pressed():
 	print("Botão pressionado")
@@ -23,11 +29,12 @@ func _on_cadastrar_pressed():
 	
 	gerenciadorJogadores.salvar_usuario(nome)
 	print(nome)
+	buttonCadastroJogador.disabled = true
 	feedbackCadastro.text = "Cadastro realizado com sucesso!"
 	feedbackCadastro.modulate = Color(0,1,0)
 	
-	nomeJogador.text = ""
+	nomeJogador.text = nome
+	nomeJogador.editable = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
