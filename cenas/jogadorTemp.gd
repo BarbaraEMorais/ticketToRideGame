a4618e9d70f63d75c086b5809748a161df7026c3
C. Goncalves <cgoncalves@id.uff.br>
2025-05-27 19:01:50 -0300

    Cria base para jogador

---
+++ cenas/jogadorTemp.gd
@@ -0,0 +1,15 @@
+# Placeholder para a classe jogador
+
+class_name JogadorTemp extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func jogarTurno() -> void:
+	pass
