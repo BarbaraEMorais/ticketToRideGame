e816506d1367c3de65f3608f922318803d1f279a
C. Goncalves <cgoncalves@id.uff.br>
2025-05-31 16:13:47 -0300

    Added label text. Added detinations JSON.

--- cartaDestino.gd
+++ cartaDestino.gd
@@ -7,9 +7,19 @@ var completado: bool
 var cidade_origem: String
 var cidade_destino: String
 
+@onready var LabelOrigem = $"Area2D/Sprite2D/Origem"
+@onready var LabelDestino = $"Area2D/Sprite2D/Destino"
+@onready var LabelPontos = $"Area2D/Sprite2D/Pontuação"
+
 func _init(origem : String, destido: String, _pontos: int=0, ) -> void:
 	pontos= _pontos
 	cidade_origem = origem
 	cidade_destino = cidade_destino
+	
 	completado = false
 	
+	
+func _ready() -> void:
+	LabelOrigem.text = cidade_origem.capitalize()
+	LabelDestino.text = cidade_destino.capitalize()
+	LabelPontos.text = pontos
