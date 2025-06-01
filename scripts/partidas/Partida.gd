d0fc4e2f0f094a8c0a8a3bca8bb3fab32ccdfeb3
João Victor Miranda <joaovictormiranda.99@gmail.com>
2025-06-01 19:13:25 -0300

    Corrigido comportamento do botão Iniciar Partida em Cadastro.gd

--- scripts/partidas/Partida.gd
+++ scripts/partidas/Partida.gd
@@ -1,20 +1,8 @@
 extends Node
 
-@onready var buttonStart = $Start
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	buttonStart.pressed.connect(_on_start_pressed)
-	
-
-func _on_start_pressed():
-	print("cliquei no botao")
-	inicia_partida()
-
-func inicia_partida():
-	print(get_parent().get_nomeJogador())
-	print(get_parent().get_nomeBots())
-
+	pass
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
