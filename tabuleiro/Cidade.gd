b4771b57a92d6d9a9391ac0a9899814e8426efa1
Alexsander Teixeira <alexsandersoarest@gmail.com>
2025-06-01 23:13:04 -0300

    feat: auto-plot map v1

--- tabuleiro/Cidade.gd
+++ tabuleiro/Cidade.gd
@@ -1,9 +1,9 @@
-class_name Destino extends Area2D
+class_name Cidade extends Area2D
 
-signal destination_clicked(destination_id: int, destination_name_val: String, node_instance: Area2D)
+signal cidade_clicked(cidade_id: int, cidade_name_val: String, node_instance: Area2D)
 
-@export var destination_id: int = 0
-@export var destination_name: String = "Unnamed Destination" : set = set_destination_name
+@export var cidade_id: int = 0
+@export var cidade_name: String = "destino sem nome" : set = set_cidade_name
 @export var map_x: float = 0.0
 @export var map_y: float = 0.0
 
@@ -14,7 +14,7 @@ func _ready():
     self.position = Vector2(map_x, map_y)
 
     if label_node:
-        label_node.text = destination_name
+        label_node.text = cidade_name
     else:
         push_warning("Destination.gd: Label node not found or not named 'Label'.")
 
@@ -23,22 +23,22 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
     """Handles input events on the Area2D."""
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            print("Clicked on Destination: ID - %s, Name - '%s' at %s" % [destination_id, destination_name, position])
-            emit_signal("destination_clicked", destination_id, destination_name, self)
+            print("Clicked on Destination: ID - %s, Name - '%s' at %s" % [cidade_id, cidade_name, position])
+            emit_signal("cidade_clicked", cidade_id, cidade_name, self)
             get_viewport().set_input_as_handled()
 
 
-func set_destination_name(new_name: String):
-    destination_name = new_name
+func set_cidade_name(new_name: String):
+    cidade_name = new_name
     if label_node:
-        label_node.text = destination_name
+        label_node.text = cidade_name
     elif is_inside_tree() and get_node_or_null("Label"):
-        get_node("Label").text = destination_name
+        get_node("Label").text = cidade_name
 
 
-func setup_data(id_val: int, name_val: String, pos_val: Vector2):
-    destination_id = id_val
-    set_destination_name(name_val)
+func setup_cidade(id_val: int, name_val: String, pos_val: Vector2):
+    cidade_id = id_val
+    set_cidade_name(name_val)
 
     map_x = pos_val.x
     map_y = pos_val.y
---
+++ tabuleiro/MapManager.gd
@@ -0,0 +1,52 @@
+class_name MapManager extends Node2D
+
+@export var cidade_scene: PackedScene
+@export var caminho_scene: PackedScene
+
+var cidades: Dictionary = {}
+var caminhos: Array[Caminho] = []
+
+func _ready():
+	_cria_varias_cidades_exemplo()
+	_cria_caminho(1, 2, 6, 2)
+	_cria_caminho(2, 3, 3, 1)
+	_cria_caminho(1, 3, 5, 3)
+
+
+func _cria_varias_cidades_exemplo():
+	var cidades_dados = [
+		{ "id": 1, "name": "North Gate", "pos": Vector2(100, 50) },
+		{ "id": 2, "name": "South Market", "pos": Vector2(500, 450) },
+		{ "id": 3, "name": "East Woods", "pos": Vector2(700, 200) }
+	]
+	for dados in cidades_dados:
+		_cria_cidade(dados["id"], dados["name"], dados["pos"])
+
+
+func _cria_cidade(id: int, cidade_name: String, pos: Vector2):
+	if cidade_scene:
+		var cidade_node = cidade_scene.instantiate() as Cidade
+		cidade_node.setup_cidade(id, cidade_name, pos)
+		add_child(cidade_node)
+		cidades[id] = cidade_node
+		print("Created cidade: ", cidade_node.cidade_name)
+	else:
+		push_warning("MapManager: cidadeino scene não setada.")
+
+
+func _cria_caminho(origem_id: int, cidade_id: int, length: int, lines: int):
+	if not caminho_scene:
+		push_warning("MapManager: Caminho scene não setada.")
+		return
+
+	if not cidades.has(origem_id) or not cidades.has(cidade_id):
+		push_warning("MapManager: Não deu pra criar o caminho. Tá faltando o cidade ou a origem.")
+		return
+
+	var origem_node: Cidade = cidades[origem_id]
+	var cidade_node: Cidade = cidades[cidade_id]
+
+	var caminho_node = caminho_scene.instantiate() as Caminho
+	caminho_node.setup_caminho(origem_node, cidade_node, length, lines)
+	add_child(caminho_node)
+	caminhos.append(caminho_node)
