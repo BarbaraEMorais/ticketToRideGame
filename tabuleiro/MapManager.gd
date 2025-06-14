0c90a419816d89db3aae8d70f77e62867d74d5a4
Alexsander Teixeira <alexsandersoarest@gmail.com>
2025-06-14 17:01:29 -0300

    feat: cities correctly positioned on map

--- tabuleiro/MapManager.gd
+++ tabuleiro/MapManager.gd
@@ -11,12 +11,7 @@ var caminhos: Array[Caminho] = []
 
 func _ready():
 	image_parser.image_saved.connect(_on_image_saved)
-
 	json_map_parser("res://dados/mapa-brasil.json")
-
-	_cria_varias_cidades_exemplo()
-	_cria_caminho(1, 2, 5, 1, ["Red"])
-	_cria_caminho(2, 3, 3, 3, ["Green", "DarkGray", "Blue"], 30)
 
 
 func _cria_varias_cidades_exemplo():
@@ -33,24 +28,25 @@ func _cria_cidade(id: int, cidade_name: String, pos: Vector2):
 	if cidade_scene:
 		var cidade_node = cidade_scene.instantiate() as Cidade
 		cidade_node.setup_cidade(id, cidade_name, pos)
+		cidade_node.scale = Vector2(0.5, 0.5)
 		add_child(cidade_node)
 		cidades[id] = cidade_node
-		print("Created cidade: ", cidade_node.cidade_name)
+		# print("Created cidade: ", cidade_node.cidade_name)
 	else:
 		push_warning("MapManager: Cidade scene não setada.")
 
 
-func _cria_caminho(origem_id: int, cidade_id: int, length: int, lines: int, colors: Array[String], curvature = 0):
+func _cria_caminho(origem_id: int, destino_id: int, length: int, lines: int, colors: Array[String], curvature = 0):
 	if not caminho_scene:
 		push_warning("MapManager: Caminho scene não setada.")
 		return
 
-	if not cidades.has(origem_id) or not cidades.has(cidade_id):
+	if not cidades.has(origem_id) or not cidades.has(destino_id):
 		push_warning("MapManager: Não deu pra criar o caminho. Tá faltando o destino ou a origem.")
 		return
 
 	var origem_node: Cidade = cidades[origem_id]
-	var cidade_node: Cidade = cidades[cidade_id]
+	var cidade_node: Cidade = cidades[destino_id]
 
 	var caminho_node = caminho_scene.instantiate() as Caminho
 	caminho_node.setup_caminho(origem_node, cidade_node, length, lines, colors, curvature)
@@ -72,8 +68,18 @@ func json_map_parser(file_path: String):
 	if !json_result:
 		printerr("Erro no parsing do JSON do mapa: ", json_result.get_error_message())
 
+	# plano de fundo
 	var output_path = "user://decoded_image.png"
 	image_parser.parseImage(json_result["detailedBackgrounds"]["skyNoRoads"], output_path)
+
+	var width = (json_result["outline"]["width"] + json_result["outline"]["offset_x"]) * 2
+	var height = (json_result["outline"]["height"] + json_result["outline"]["offset_y"]) * 2
+	var dimensions = json_result["dimensions"]
+	# cidades
+	for cidade in json_result["destinations"]:
+		print(cidade)
+		_cria_cidade(cidade["id"], cidade["name"], Vector2(-width/2 + cidade["x"]*width / dimensions[0], height/2 + -cidade["y"] * height))
+
 
 
 func _on_image_saved(image_path: String):
