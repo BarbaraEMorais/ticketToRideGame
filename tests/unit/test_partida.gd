02b347960364c05f61cb8a42b7211ad447926220
João Victor Miranda <joaovictormiranda.99@gmail.com>
2025-06-27 10:31:20 -0300

    Fixed unit test for set_partida method

--- tests/unit/test_partida.gd
+++ tests/unit/test_partida.gd
@@ -2,19 +2,19 @@ extends "res://addons/gut/test.gd"
 
 var jogador = load("res://scripts/jogadores/jogador.gd")
 
-var partida = load("res://cenas/partida.tscn")
+var cena_partida = load("res://cenas/partida.tscn")
 
 func test_set_partida():
-	var partida_instancia = partida.instantiate()
-	add_child(partida_instancia)
+	var instancia = partial_double(cena_partida).instantiate()
+	add_child(instancia)
 	var nomes: Array[String] = ["Humano", "Bot1", "Bot2"]
 	var cor_humano = "vermelho"
 
-	partida_instancia.set_partida(nomes, cor_humano)
+	instancia.set_partida(nomes, cor_humano)
 
-	assert_eq(partida_instancia.listaJogadores.size(), 3, "listaJogadores deveria ter 3 jogadores")
+	assert_eq(instancia.listaJogadores.size(), 3, "listaJogadores deveria ter 3 jogadores")
 
-	var jogador_humano = partida_instancia.listaJogadores[0] 
+	var jogador_humano = instancia.listaJogadores[0]
 
 	assert_is(jogador_humano, Jogador, "Humano deveria ser do tipo Jogador")
 	assert_eq(jogador_humano.get_nome(), "Humano", "O nome do jogador_humano deveria ser 'Humano'")
@@ -22,6 +22,4 @@ func test_set_partida():
 	assert_eq(jogador_humano.get_mao().position, Vector2(980, 1015), "Mão do jogador humano deveria estar na posição (980, 1015)")
 	assert_eq(jogador_humano.get_status().position, Vector2(192, 990), "Card de status do jogador humano deveria estar na posição (192, 990)")
 	
-	var partida_db = double(partida.instantiate())
-	assert_called_count(partida_db.add_child(), 3)
-	assert_not_null(partida_instancia.mesa)
+	assert_not_null(instancia.mesa)
